c18c3e94530443525e2c76d6f4f77a442e4af23f
Francisco Palomeque <franciscopalomeque@MacBook-Pro-de-Francisco.local>
2022-07-03 01:22:04 -0300

    add normalize css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "../components/NavBarTop/navStyles.css";
+import "../styles/normalize.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }

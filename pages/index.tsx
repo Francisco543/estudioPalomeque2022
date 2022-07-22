e747e2d87d48e719007ed55b6f9f37ae36b265d0
Francisco543 <palito10fran@gmail.com>
2022-07-21 23:05:02 -0300

    probando

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ const Home: NextPage = () => {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-
         <link
           href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap"
           rel="stylesheet"

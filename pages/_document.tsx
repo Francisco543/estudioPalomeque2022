d429d171316c2d19135ee0d2d6a357cb231c78bb
Francisco543 <palito10fran@gmail.com>
2022-07-23 18:29:42 -0300

    fix build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -62,7 +62,7 @@ class MyDocument extends Document {
             content="Estudio juridico Palomeque, Eugenia Palomeque, abogada especializada en Compliance. Revisor Externo Independiente, Auditoria, Diseño de procesos, Comite. "
           />
           <meta name="description" content="Estudio juridico Palomeque" />
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta
             name="viewport"
             content="initial-scale=1.0, user-scalable=yes, viewport-fit=cover"
